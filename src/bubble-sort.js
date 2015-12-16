7787f2c64ad89381abe40164a76c07b85fba915f
rodson <rodsonluo@gmail.com>
2015-12-16 09:10:28 +0800

    fix bubble sort

--- src/bubble-sort.js
+++ src/bubble-sort.js
@@ -3,12 +3,19 @@ export const bubbleSort = (input) => {
   const length = output.length;
 
   for (let i = length - 1; i > 0; i--) {
+    let swapped = false;
+
     for (let j = 1; j <= i; j++) {
       if (output[j - 1] > output[j]) {
+        swapped = true;
         let temp = output[j];
         output[j] = output[j - 1];
         output[j - 1] = temp;
       }
+    }
+
+    if (!swapped) {
+      break;
     }
   }
 
